13a40c3c00d459181fd53e75254792bd52e0763d
BYIRINGIRO ALOYS <aloys@kali>
2025-05-21 13:24:44 +0200

    working on hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -15,18 +15,27 @@ const Hero = () => {
         speed: 500,
         SlidesToShow: 1,
         slidesToScroll: 1,
-        arrows: false,
+        // arrows: false
     };
 
-    useEffect(()=>{
+    useEffect(() => {
         AOS.init({
-            offset:100,
-            duration:500, 
-        }); 
+            offset: 100,
+            duration: 500,
+            easing: 'ease-in-out'
+        });
+        AOS.refresh();
     })
+
     return (
-        <div>
-            heros section
+        <div id='hero' className='w-full flex justify-center items-center lg:h-[700px] h-[600px]'>
+            <Slider className='w-full' {...settings}>
+                <div>
+                    <div className='w-full lg:px-20 px-5 lg:h-[700px] flex flex-col justify-center items-start gap-10 bg-cover  bg-no-repeat bg-center' style={{ backgroundImage: `url(${dslr})`}}>
+                        <h1 data-aos='zoom-in' data-aos-delay='50' className=''></h1>
+                    </div>
+                </div>
+            </Slider>
         </div>
     )
 }
